0f8336cf72ea295efffa5bccb5a0819687ea2151
lin <17789612632@163.com>
2019-10-31 14:25:06 +0800

    develop第一次提交

---
+++ aaaaa/src/main/java/Test.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<String> objects = Arrays.asList("1","2","3");
+        for (String object : objects) {
+            System.out.println(object);
+        }
+    }
+}
